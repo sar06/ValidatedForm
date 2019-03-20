8767c07ffaacf6734f16bf8cd6442e75a3651375
sar06 <48696842+sar06@users.noreply.github.com>
2019-03-20 14:22:49 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import "./App.css"
+import ValidatedFormPage from "./container/ValidatedFormPage"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ValidatedFormPage />
+      </div>
+    )
+  }
+}
+
+export default App
